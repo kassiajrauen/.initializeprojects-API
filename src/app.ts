77d40d28aa1998554b5df5c7518c7f49548ce6fe
KassiaJRauen <kassia.grj@gmail.com>
2022-07-25 17:06:41 -0300

    feat: add handlerError

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import "express-async-errors";
+import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
 
 dotenv.config();
 
 const app = express();
 app.use(json());
 app.use(cors());
+app.use(errorHandlerMiddleware);
 
 export default app;
